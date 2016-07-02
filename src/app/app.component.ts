8be2202bf524dd724a8e820816a48efc3c48de5b
airburst <mark@fairhursts.net>
2016-07-02 11:20:39 +0100

    Setup repo and added material basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
+import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
 
 @Component({
   moduleId: module.id,
