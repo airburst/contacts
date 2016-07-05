ad7e53d524e5d8dd9c3d993f5e3fd35198f016b1
airburst <mark@fairhursts.net>
2016-07-05 12:25:05 +0100

    Stub methods for CRUD in contacts.service

--- src/app/contacts.service.ts
+++ src/app/contacts.service.ts
@@ -4,20 +4,32 @@ import {
   FirebaseListObservable,
   FirebaseObjectObservable
 } from 'angularfire2';
-import { Contact } from './contact';
+import { IContact } from './contact';
 
 @Injectable()
 export class ContactsService {
 
-  contacts$: FirebaseListObservable<Contact[]>;
+  contacts$: FirebaseListObservable<IContact[]>;
 
   constructor(private af: AngularFire) {
     this.contacts$ = af.database.list('/contacts');
   }
 
-  contact$(id: string): FirebaseObjectObservable<Contact> {
+  contact$(id: string): FirebaseObjectObservable<IContact> {
     let url:string = '/contacts/' + id;
     return this.af.database.object(url);
+  }
+
+  add(contact: IContact) {
+    // Add new record into contacts collection
+  }
+
+  update(contact) {
+    // Push update for contact record
+  }
+
+  remove(contactId) {
+    // Delete contact record
   }
 
 }
